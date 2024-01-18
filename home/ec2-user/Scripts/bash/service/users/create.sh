a0722279bd824f71f51ca2da45eab1066f8c5760
Martynas Stake <martynas.stake@gmail.com>
2024-01-18 12:29:55 +0200

    feat(install-scripts): Fixing bugs

--- home/ec2-user/Scripts/bash/service/users/create.sh
+++ home/ec2-user/Scripts/bash/service/users/create.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-set_aws_ec2_service_user_password() {
+function set_aws_ec2_service_user_password() {
     local password=$(date | base64)
     echo $password | sudo passwd $1 --stdin
 }
 
-set_aws_ec2_instance_service_user_sftp_directory() {
+function set_aws_ec2_instance_service_user_sftp_directory() {
     local user_directory=$AWS_EC2_INSTANCE_SFTP_DIRECTORY/$1
     local wars_directory=$AWS_EC2_INSTANCE_SFTP_DIRECTORY/$1/wars
 
     sudo mkdir -p $wars_directory
-    sudo chown $1:$AWS_EC2_INSTANCE_SFTP_GROUP_NAME $wars_directory
-    sudo chown root:$AWS_EC2_INSTANCE_SFTP_GROUP_NAME $user_directory
+    sudo chown $1:$2 $wars_directory
+    sudo chown root:$2 $user_directory
     sudo chmod -R o+rx $user_directory
     sudo restorecon -F -R $user_directory
 }
 
-set_aws_ec2_instance_service_user_ssh_access() {
+function set_aws_ec2_instance_service_user_ssh_access() {
     sudo cp -r $HOME/.ssh /opt/$1/
     sudo chown -R $1:$1 /opt/$1/.ssh
     sudo restorecon -F -R /opt/$1/.ssh
@@ -29,5 +29,5 @@ for aws_ec2_instance_service_user_name in "${AWS_EC2_INSTANCE_SERVICE_USER_NAMES
         -aG $AWS_EC2_INSTANCE_SFTP_GROUP_NAME $aws_ec2_instance_service_user_name
     set_aws_ec2_service_user_password $aws_ec2_instance_service_user_name
     set_aws_ec2_instance_service_user_ssh_access $aws_ec2_instance_service_user_name
-    set_aws_ec2_instance_service_user_sftp_directory $aws_ec2_instance_service_user_name
+    set_aws_ec2_instance_service_user_sftp_directory $aws_ec2_instance_service_user_name $AWS_EC2_INSTANCE_SFTP_GROUP_NAME
 done
