196dfa405091366e4829bc0c0df89572ad7c264d
Martynas Stake <martynas.stake@gmail.com>
2024-01-23 13:10:12 +0200

    fix(conf): Problems with envsubst

--- home/ec2-user/Scripts/bash/nginx/function/copy_iw_app_nginx_configuration.sh
+++ home/ec2-user/Scripts/bash/nginx/function/copy_iw_app_nginx_configuration.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 function copy_iw_app_nginx_configuration() {
-    local iw_app_domain=${IW_TOMCAT_SERVICE_DOMAIN[$1]}
-    local iw_app_port=${IW_TOMCAT_SERVICE_PORT[$1]}
-    local iw_app_ui_path=/var/www/html/$iw_app_domain
-    local iw_app_conf_path=/etc/nginx/conf.d/$iw_app_domain.conf
+    export readonly IW_APP_DOMAIN=${IW_TOMCAT_SERVICE_DOMAIN[$1]}
+    export readonly IW_APP_PORT=${IW_TOMCAT_SERVICE_PORT[$1]}
+    local iw_app_ui_path=/var/www/html/$IW_APP_DOMAIN
+    local iw_app_conf_path=/etc/nginx/conf.d/$IW_APP_DOMAIN.conf
 
     sudo mkdir -p $iw_app_ui_path
     sudo chown nginx:nginx $iw_app_ui_path
     sudo restorecon -F -R $iw_app_ui_path
 
-    envsubst < $IW_SCRIPT_CONF_DIR/etc/nginx/conf.d/nginx.template.conf > $IW_SCRIPT_CONF_DIR$iw_app_conf_path
+    envsubst '$IW_APP_DOMAIN,$IW_APP_PORT' < $IW_SCRIPT_CONF_DIR/etc/nginx/conf.d/nginx.template.conf > $IW_SCRIPT_CONF_DIR$iw_app_conf_path
     sudo mv $IW_SCRIPT_CONF_DIR$iw_app_conf_path $iw_app_conf_path
     sudo chown root:root $iw_app_conf_path
     sudo restorecon -F -R $iw_app_conf_path
 
-    echo "127.0.0.1 $iw_app_domain" | sudo tee --append /etc/hosts
+    echo "127.0.0.1 $IW_APP_DOMAIN" | sudo tee --append /etc/hosts
 
     sudo systemctl restart nginx
+    unset IW_APP_DOMAIN
+    unset IW_APP_PORT
 }
