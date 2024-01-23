661aa5acb9381a0fb53055aa490c97c0915d74dc
Martynas Stake <martynas.stake@gmail.com>
2024-01-23 12:46:37 +0200

    fix(conf): More NGINX problems

--- home/ec2-user/Scripts/bash/nginx/configure.sh
+++ home/ec2-user/Scripts/bash/nginx/configure.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo cp $IW_SCRIPT_CONF_DIR/etc/nginx/conf.d/gzip.conf /etc/nginx/conf.d/
+sudo restorecon -F -R /etc/nginx/conf.d/
--- home/ec2-user/Scripts/bash/nginx/function/copy_iw_app_nginx_configuration.sh
+++ home/ec2-user/Scripts/bash/nginx/function/copy_iw_app_nginx_configuration.sh
@@ -9,7 +9,7 @@ function copy_iw_app_nginx_configuration() {
     sudo chown nginx:nginx $iw_app_ui_path
     sudo restorecon -F -R $iw_app_ui_path
 
-    envsubstr < $IW_SCRIPT_CONF_DIR/etc/nginx/conf.d/nginx.template.conf > $IW_SCRIPT_CONF_DIR$iw_app_conf_path
+    envsubst < $IW_SCRIPT_CONF_DIR/etc/nginx/conf.d/nginx.template.conf > $IW_SCRIPT_CONF_DIR$iw_app_conf_path
     sudo mv $IW_SCRIPT_CONF_DIR$iw_app_conf_path $iw_app_conf_path
     sudo chown root:root $iw_app_conf_path
     sudo restorecon -F -R $iw_app_conf_path
