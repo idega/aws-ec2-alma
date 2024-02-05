1165646e27a516a4872ca102aa86bb92db216d3f
Martynas Stake <martynas.stake@gmail.com>
2024-02-05 12:33:50 +0200

    feat(nginx): Adding default configuration for unknown hosts

--- home/ec2-user/Scripts/bash/nginx/configure.sh
+++ home/ec2-user/Scripts/bash/nginx/configure.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo cp $IW_ALMA_CONFIGURATION_HOME/etc/nginx/conf.d/gzip.conf /etc/nginx/conf.d/
+sudo cp $IW_ALMA_CONFIGURATION_HOME/etc/nginx/conf.d/default.conf /etc/nginx/conf.d/
 sudo restorecon -F -R /etc/nginx/conf.d/
