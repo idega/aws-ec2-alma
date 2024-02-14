bf1e3bcfd1cf12ea8c7cd1d1ae17b3b68c94f70d
Martynas Stake <martynas.stake@gmail.com>
2024-02-14 18:28:00 +0200

    fix(update-ui): Wrong file

--- home/ec2-user/Scripts/bash/s3/function/update_iw_ui_source.sh
+++ home/ec2-user/Scripts/bash/s3/function/update_iw_ui_source.sh
@@ -2,6 +2,6 @@
 function update_iw_ui_source() {
     s3cmd put * s3://$1/ --recursive
     aws s3api put-object --bucket $1 --key index.bundle.js --body index.bundle.js  --content-type application/javascript
-    aws s3api put-object --bucket $1 --key index.fonts.css --body index.bundle.js  --content-type text/css
-    aws s3api put-object --bucket $1 --key index.styles.css --body index.bundle.js  --content-type text/css
+    aws s3api put-object --bucket $1 --key index.fonts.css --body index.fonts.css  --content-type text/css
+    aws s3api put-object --bucket $1 --key index.styles.css --body index.styles.css  --content-type text/css
 }
