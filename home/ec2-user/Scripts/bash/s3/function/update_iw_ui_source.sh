f2b9c278cdd55c88b80acde99166bc4717ee7daf
Martynas Stake <martynas.stake@gmail.com>
2024-03-13 14:59:20 +0200

    fix(cloud-front): Invalidating

--- home/ec2-user/Scripts/bash/s3/function/update_iw_ui_source.sh
+++ home/ec2-user/Scripts/bash/s3/function/update_iw_ui_source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function update_iw_ui_source() {
-    s3cmd put * s3://$1/ --recursive
+    s3cmd put * s3://$1/ --recursive --cf-invalidate
     aws s3api put-object --bucket $1 --key index.bundle.js --body index.bundle.js  --content-type application/javascript
     aws s3api put-object --bucket $1 --key index.fonts.css --body index.fonts.css  --content-type text/css
     aws s3api put-object --bucket $1 --key index.styles.css --body index.styles.css  --content-type text/css
