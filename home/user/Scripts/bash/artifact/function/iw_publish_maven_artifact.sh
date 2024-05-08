d537becca2a4aec5398e82f8b5e07df7f327ab40
Martynas Stake <martynas.stake@gmail.com>
2024-05-08 14:45:51 +0300

    fix(artifact): All files are being uploaded

--- home/user/Scripts/bash/artifact/function/iw_publish_maven_artifact.sh
+++ home/user/Scripts/bash/artifact/function/iw_publish_maven_artifact.sh
@@ -8,7 +8,6 @@ export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domai
 function iw_put_maven_artifact {
     local iw_maven_artifact_location="$1"
     local iw_maven_artifact_path=${iw_maven_artifact_location#*repository/}
-    echo "Uploading: $iw_maven_artifact_path"
 
     curl --request PUT https://$IW_CA_DOMAIN-$IW_CA_DOMAIN_OWNER.d.codeartifact.$IW_CA_DOMAIN_REGION.amazonaws.com/maven/$IW_CA_REPO_NAME/$iw_maven_artifact_path \
         --user "aws:$CODEARTIFACT_AUTH_TOKEN" \
@@ -18,10 +17,7 @@ function iw_put_maven_artifact {
 
 function iw_publish_maven_artifact {
     # Removing suffix .jar or .pom
-    local iw_maven_artifact_location="${1%.*}"
-
-    # Extracting exact filename, like commons.jar
-    local iw_maven_artifact_file_name=${iw_maven_artifact_location##*/}
+    local iw_maven_artifact_location=$1
 
     # Extracting path to file starting folder in maven repo. All paths should start with "repository/"
     local iw_maven_artifact_repo_path=${iw_maven_artifact_location#*repository/}
@@ -46,15 +42,15 @@ function iw_publish_maven_artifact {
 
 
     echo "Maven artifact path: $iw_maven_artifact_location"
-    echo "Maven artifact filename: $iw_maven_artifact_file_name"
     echo "Maven artifact repo path: $iw_maven_artifact_repo_path"
     echo "Maven artifact group id: $iw_maven_artifact_group_id"
     echo "Maven artifact id: $iw_maven_artifact_id"
     echo "Maven artifact version: $iw_maven_artifact_version"
 
-    iw_put_maven_artifact $iw_maven_artifact_location.pom
-    iw_put_maven_artifact $iw_maven_artifact_location.jar
-    iw_put_maven_artifact $iw_maven_artifact_location-sources.jar
+    for iw_maven_artifact_filename in $iw_maven_artifact_location*; do
+        echo "Uploading: $iw_maven_artifact_filename"
+        iw_put_maven_artifact $iw_maven_artifact_filename
+    done
 
     aws codeartifact update-package-versions-status \
         --domain $IW_CA_DOMAIN \
@@ -67,6 +63,3 @@ function iw_publish_maven_artifact {
         --target-status Published \
         --region $IW_CA_DOMAIN_REGION
 }
-
-
-
