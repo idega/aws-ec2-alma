d807237df1a17890ecb054ae88a62ca5a3a3c917
Martynas Stake <martynas.stake@gmail.com>
2024-06-14 12:22:04 +0300

    refactor(update): Optimizing update scripts

--- opt/app/tomcat/bin/update.sh
+++ opt/app/tomcat/bin/update.sh
@@ -3,6 +3,9 @@ function install_iw_app_war_file() {
     local iw_tomcat_home_dir=/opt/$1/tomcat
     local iw_service_user_sftp_dir=/var/local/sftp/$1/wars
 
+    echo "Backing up"
+    tar -cvf $iw_tomcat_home_dir/webapps.$(date +%Y-%m-%d).tar.gz $iw_tomcat_home_dir/webapps
+
     for iw_war_file_path in $(ls $iw_service_user_sftp_dir/*.war); do
         if [ -f "$iw_war_file_path" ]; then
             iw_war_file_name="$(basename "$iw_war_file_path" .war)"
@@ -14,19 +17,18 @@ function install_iw_app_war_file() {
             echo "Extracting $iw_war_file_path file..."
             unzip $iw_war_file_path -d $iw_tomcat_home_dir/webapps/$iw_war_file_name
 
-            echo "Touching files..."
-            find $iw_tomcat_home_dir/webapps/$iw_war_file_name ! -name . -prune -type d -exec touch {} +
-
-            echo "Restoring reading rights..."
-            chown -R $1:$1 $iw_tomcat_home_dir/webapps/$iw_war_file_name
-            restorecon -F -R $iw_tomcat_home_dir/webapps/$iw_war_file_name
-
             echo "Removing $iw_war_file_path file..."
             rm -f $iw_war_file_path
         fi
     done
-}
 
+    echo "Touching files..."
+    find $iw_tomcat_home_dir/webapps/ -type f -exec touch {} +
+
+    echo "Restoring reading rights..."
+    chown -R $1:$1 $iw_tomcat_home_dir/webapps/
+    restorecon -F -R $iw_tomcat_home_dir/webapps/
+}
 
 install_iw_app_war_file $(id -un)
 ./opt/$(id -un)/tomcat/bin/startup.sh
